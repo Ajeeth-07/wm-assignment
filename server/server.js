3778b45d0fcc00a13abed47d94b88671071883bc
Ajeeth Shinde <89684651+Ajeeth-07@users.noreply.github.com>
2025-03-29 11:19:11 +0530

    server.js file

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+
+//routes
+// const authRoutes = require('./routes/authRoutes');
+// const draftRoutes = require('./routes/draftRoutes');
+// const driveRoutes = require('./routes/driveRoutes');
+
+
+//firebase admin
+// require('./config/firebase');
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//routes
+app.use('/api/auth', authRoutes);
+app.use('/api/drafts', draftRoutes);
+app.use('/api/drive', driveRoutes);
+
+//error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.statusCode || 500).json({
+        error : err.message || 'Internal Server Error'
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
