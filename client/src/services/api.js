67700477ec142465b6592ac328bc24aed2bf5fbf
Ajeeth Shinde <89684651+Ajeeth-07@users.noreply.github.com>
2025-03-29 23:55:54 +0530

    updated env

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://wm-assignment-backend.onrender.com";
 
 // Create axios instance with default config
 const apiClient = axios.create({
